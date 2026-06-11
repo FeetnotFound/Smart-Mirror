442b9802217d7edd584dfaa1e5a62302b74f3fd4
FeetnotFound <theodor.f.schermann@gmail.com>
2026-06-10 20:43:47 -0700

    better installer script

--- setup_mirror_os.sh
+++ setup_mirror_os.sh
@@ -49,6 +49,7 @@ apt-get install -y \
   xorg xinit x11-xserver-utils \
   libxcb-xinerama0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 \
   libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-xfixes0 \
+  libxcb-cursor0 libxcb-xkb1 libxkbcommon-x11-0 \
   libgl1-mesa-glx libglib2.0-0 \
   fonts-dejavu-core fontconfig \
   avahi-daemon avahi-utils libnss-mdns \
